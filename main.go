ce6b255208d91d4bebd6971d33483f939cc0a93d
Dominic Foli <codemaestro@Dominics-MacBook-Pro.local>
2024-04-20 09:16:25 +0000

    Solution 1 impl

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	fileNamePtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer' (default \"problems.csv\")")
+	flag.Parse()
+
+	quizzes := readQuizFile(*fileNamePtr)
+	score := setQuiz(quizzes)
+
+	fmt.Printf("You scored %d out of %d.", score, len(quizzes))
+
+}
+
+func readQuizFile(fileName string) map[string]int {
+	quizzes := make(map[string]int)
+
+	// os.open opens a specific file in
+	// read only mode and this return
+	// a pointer of type os.File
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln("Error while reading file:", err)
+	}
+
+	// close the file
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalln("Error occurred while closing file:", err)
+		}
+	}(file)
+
+	// The csv.NewReader() function is called in
+	// which the object os.File passed as its parameters
+	// and this creates a new csv reader that reads
+	//from the file
+	reader := csv.NewReader(file)
+
+	// ReadAll reads all the records from the CSV file
+	// and Returns them as slice of slices of string
+	// and an error if any
+	records, err := reader.ReadAll()
+
+	// Checks for the error
+	if err != nil {
+		log.Fatalln("Error while reading records", err)
+	}
+
+	// Loop to iterate through
+	// and print each of the string slice
+	for _, record := range records {
+		quizResult, err := strconv.Atoi(record[1])
+		if err != nil {
+			log.Fatalf("Cannot get result for %d", record[0], err)
+		}
+		quizzes[record[0]] = quizResult
+	}
+	return quizzes
+}
+
+func setQuiz(quizzes map[string]int) int {
+	score := 0
+	quizCount := 0
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Simple Quiz")
+	fmt.Println("============")
+
+	for k, v := range quizzes {
+		quizCount++
+		fmt.Printf("Problem #%d: %v = ", quizCount, k)
+		text, _ := inputReader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		answer, err := strconv.Atoi(text)
+		if err != nil {
+			continue
+		}
+		if answer == v {
+			score++
+		}
+	}
+	return score
+}
